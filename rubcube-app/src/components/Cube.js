215696f32ba3d85cc1fbcdcdb7f756061588efa6
Karina Rodriguez <karminer60@gmail.com>
2021-04-11 20:19:59 -0700

    created three squares to build the Rubik's cube

--- rubcube-app/src/components/Cube.js
+++ rubcube-app/src/components/Cube.js
@@ -7,9 +7,20 @@ const Cube = () => {
     const [state, setState] = useState(null)
     return(
         <div>
-            <div>
+            <h2> Cube Visualization Goes Here </h2>
+            
+            <div id="cubeSide1" style={{ paddingLeft: "44.5%" }}>
+                <CubeDetails/>   
+            </div>
+            <div id="cubeSide2" style={{ paddingLeft: "44.5%", transform: `skew(20deg, 20deg)`}}>
                 <CubeDetails/> 
             </div>
+
+            <div id="cubeSide3" style={{ paddingLeft: "44.5%", transform: `skew(20deg, 20deg)`}}>
+                <CubeDetails/> 
+            </div>
+                
+            
             <button id= 'initial-botton' >
                 Button   
             </button>
--- rubcube-app/src/components/CubeDetails.js
+++ rubcube-app/src/components/CubeDetails.js
@@ -5,34 +5,33 @@ import { connect } from 'react-redux'
 
 const CubeDetails = () => {
     const [state, setState] = useState(null)
+    const z1 = '100px'
+    const y1 = '50px'
+    const z2 ='100px'
+    const x2 = '0px'
+    
     return (
         <div id="cubeRepresentation">
             {/*I have to build a 2D object with three axis, each with three cube sides*/}
             {/* How can I build this: 
           1. Build x, y, z axes
-            a. crate 2D representation
+            a. create 2D representation
           2. Add three 2D squares to each axis*/}
             <div id="visualization">
-                <h2> Cube Visualization Goes Here </h2>
-                <div id="cube" style={{ paddingLeft: "44.5%" }}>
-                    <div style={{ border: "solid", borderColor: "black", width: "9rem", height: "9rem", display: "flex", flexWrap: "wrap" }}>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
-                        <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                
+                <div style={{ border: "solid", borderColor: "black", width: "9rem", height: "9rem", display: "flex", flexWrap: "wrap" }}>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
+                    <div style={{ border: "solid", borderColor: "black", width: "2.62rem", height: "2.62rem" }}></div>
 
 
-                    </div>
                 </div>
-
-
-
-
 
             </div>
 
