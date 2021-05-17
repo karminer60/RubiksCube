c156f2155cae4f17b1e5e3e3374118ad3ea6638e
karina-rodriguez <karminer60@gmail.com>
2021-05-16 19:19:42 -0700

    created plan for moves algorithm

--- rubcube-app/src/components/Cube.js
+++ rubcube-app/src/components/Cube.js
@@ -9,8 +9,13 @@ import { connect } from 'react-redux'
     
 Build the logic to make moves and the actual move options
 How will I do this:
-    1. Create a button that allows the user to choose between:
-        a. Twelve possible moves
+    1. Create buttons that allow the user to choose between:
+        a. 18 possible moves possible moves
+            i. Three dimensions * three rows * two ways to move = 18
+                a. change color of all two dimensional squares for each move
+                    1. create algorithm that changes multiple squares' colors according move
+                        a. store each side's current colors in state- do this by creating six arrays (six sides) with 9 squares (3 by 3)
+                        
         */
 
 const Cube = () => {
